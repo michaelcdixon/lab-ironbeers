6b7e8e06e2ed3fcd093ae73175946cde31a436cb
michaelcdixon <michaelmail7@gmail.com>
2021-08-31 17:46:38 +0100

    Working backend

--- app.js
+++ app.js
@@ -12,14 +12,36 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 // Register the location for handlebars partials here:
 
 // ...
 
 // Add the route handlers here:
 
+// app.get('/', (req, res) => {
+//   res.render('index', { index });
+// });
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beer', async (req, res) => {
+  const beer = await punkAPI.getBeers();
+  res.render('beer', { beers });
+});
+
+app.get('/random-beer', async (req, res) => {
+  const randomBeer = await punkAPI.getRandom();
+  res.render('random-beer', { randomBeer });
+});
+
+//Modern way to consume promises - Async JS!
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers();
+  res.render('beers', { beers });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
